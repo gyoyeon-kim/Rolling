10d5c7eaefb04aee1c5f4df9f975379fbd15e49a
gyoyeon Kim <gy2121@naver.com>
2025-01-27 17:21:18 +0900

    Feat: From 페이지 - 보내기 버튼 생성

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -4,7 +4,7 @@ import "./Example.css";
 function Main() {
   return (
     <div className="Main">
-      <div className="Title">테스트 페이지입니다.</div>
+      <div className="Title">메인 페이지입니다.</div>
       <div>
         <p>기본 글꼴 Pretendard</p>
       </div>
