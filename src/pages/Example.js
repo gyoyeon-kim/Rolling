161009cc9286e2788472eea72bf678692da2a3b9
gyoyeon Kim <gy2121@naver.com>
2025-01-31 00:16:06 +0900

    자동 배포 테스트

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -7,6 +7,8 @@ function Main() {
       <div className="Title">메인 페이지입니다.</div>
       <div>
         <p>기본 글꼴 Pretendard</p>
+        <br />
+        <p>배포 Vercel</p>
       </div>
     </div>
   );
