3a9629c1c5d9a5ee00e6815da29e5331f1008087
Seongjun <drct9810@naver.com>
2025-01-31 01:28:00 +0900

    Fix index.js And Delete Ex File

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; // 전체 애플리케이션 적용 css
-import App from "./App";
-import reportWebVitals from "./etc/reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // 전체 애플리케이션 적용 css
+import App from './App';
+import reportWebVitals from './etc/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
