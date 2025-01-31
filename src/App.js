5531585cb1ab57972362463cfcb3bcb1267cc585
gyoyeon Kim <gy2121@naver.com>
2025-01-31 14:04:38 +0900

    보희님 머지 전

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import "./App.css";
 
 import Post_HS from "./pages/postHS";
 import From from "./pages/From";
+import Home from "./pages/HomeBH";
+import ListPageBH from "./pages/ListPageBH";
+import PostPageBH from "./pages/PostPageBH";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<ListPageBH />} />
+        <Route path="/post" element={<PostPageBH />} />
+        <Route path="/post/:id" element={<PostPageBH />} />
         <Route path="/post/123" element={<Post_HS />} />
         <Route path="/post/message" element={<From />} />
       </Routes>
