79fe9ce49f82db284d0646227ed9e56fccbde1df
gyoyeon Kim <gy2121@naver.com>
2025-01-31 15:40:11 +0900

    성준님 머지 오류 수정중

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Post_HS from "./pages/postHS";
 import From from "./pages/From";
 import Home from "./pages/Home";
 import ListPageBH from "./pages/ListPageBH";
+import PostPageBH from "./pages/PostPageBH"
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<ListPageBH />} />
+        <Route path="/post" element={<PostPageBH />} />
+        <Route path="/post/:id" element={<PostPageBH />} />
         <Route path="/post/123" element={<Post_HS />} />
         <Route path="/post/message" element={<From />} />
       </Routes>
