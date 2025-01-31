85e9628bb657ba65863cf556c4332c34194cbc06
eplssun <pretty_who@naver.com>
2025-01-31 11:29:57 +0900

    ui 작업

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Post_HS from "./pages/post_HS";
+import Post_HS from "./pages/postHS";
 
 function App() {
   return (
