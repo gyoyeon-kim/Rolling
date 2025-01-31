0ddf3d76f46337d14c30812771b0e02a89fb16c1
홍보희 <ghdqhgml833@naver.com>
2025-01-31 13:35:38 +0900

    수정: 교연님이 주신 코드

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import Post_HS from "./pages/postHS";
 import From from "./pages/From";
 import Home from "./pages/HomeBH";
 import ListPageBH from "./pages/ListPageBH";
@@ -11,11 +12,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/post/message" element={<From />} />
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<ListPageBH />} />
         <Route path="/post" element={<PostPageBH />} />
-        <Route path="/post/:id" element={<PostPageBH />} /> {}
+        <Route path="/post/:id" element={<PostPageBH />} />
+        <Route path="/post/123" element={<Post_HS />} />
+        <Route path="/post/message" element={<From />} />
       </Routes>
     </Router>
   );
