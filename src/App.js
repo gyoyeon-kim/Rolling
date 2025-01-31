6bb34138a9d0c6a39639e37883b27bacb007a7cb
홍보희 <ghdqhgml833@naver.com>
2025-01-31 13:57:40 +0900

    수정: App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Home from "./pages/HomeBH":
-import ListPageBH from "./pages/ListPageBH":
-import PostPageBH from "./pages/PostPageBH":
-import PostpageBH from "./pages/PostPageBH":
-import Post_HS from "./pages/postHS":
-import From from "./pages/From":
+import Home from "./pages/HomeBH";
+import ListPageBH from "./pages/ListPageBH";
+import PostPageBH from "./pages/PostPageBH";
+import Post_HS from "./pages/postHS";
+import From from "./pages/From";
 
 function App() {
   return (
     <Router>
       <Routes>
-      
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<ListPageBH />} />
-        <Route path="/post" element={<PostPageBH />} />  
-        <Route path="/post/:id" element={<PostPageBH />} {}/>
+        <Route path="/post" element={<PostPageBH />} />
+        <Route path="/post/:id" element={<PostPageBH />} />
         <Route path="/post/123" element={<Post_HS />} />
         <Route path="/post/message" element={<From />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
