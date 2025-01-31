00b22e855e742fb31c3147f31e84a304628ba556
Seongjun <drct9810@naver.com>
2025-01-31 13:13:19 +0900

    Fix text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
             서로에게 이모지로 감정을 표현해보세요
           </h3>
           <p className="point-description">
-            롤링 페이퍼에 이모지를 추가할 수 있어요.
+            롤링 페이퍼에 이모지를 추가할 수 있어요!
           </p>
           <div className="emoji-preview">
             <div className="emoji-display">
