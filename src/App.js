a9c7f82a49f6f833a7f160db8b47cc8f1c0f60d7
gyoyeon Kim <gy2121@naver.com>
2025-01-31 14:06:41 +0900

    Merge-보희

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Home from "./pages/HomeBH";
-import ListPageBH from "./pages/ListPageBH";
-import PostPageBH from "./pages/PostPageBH";
 import Post_HS from "./pages/postHS";
 import From from "./pages/From";
 import Home from "./pages/HomeBH";
