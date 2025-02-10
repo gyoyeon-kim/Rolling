e90c7829360bfda9e333013808058977973a2473
Seongjun <drct9810@naver.com>
2025-02-10 19:00:27 +0900

    Pc And Mobile 반응형 마무리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,83 +7,33 @@ import Navigation from "../component/Navigation";
 import point02 from "../images/Point02.png";
 import main_ex from "../images/main-ex1.jpg";
 import main_ex2 from "../images/main-ex2.jpg";
+import CardImg from "../images/CardList.svg";
+import CursorEffect from "../component/commons/CursorEffect";
 
 function Home() {
   return (
     <>
+      <CursorEffect />
       <Navigation />
       <main className="home-container">
         {/* 서비스 소개 섹션 */}
-        <section>
-          <div className="point-container">
-            <h2 className="point-title">Point.01</h2>
-            <h3 className="point-subtitle">
-              누구나 손쉽게, 온라인 <br /> 롤링 페이퍼를 만들 수 있어요
-            </h3>
-            <p className="point-description">로그인 없이 자유롭게 만들어요.</p>
-
-            {/* 왼쪽에 카드 2개 + 버튼이 있는 형태 */}
-            <div className="card-wrapper">
-              {/* 첫 번째 카드 */}
-              <div className="home-card">
-                <div className="card-header">
-                  <div className="card-profile-image">
-                    {/* 여기서 실제 이미지 경로나 URL로 변경 */}
-                    <img src={main_ex} alt="프로필1" />
-                  </div>
-                  <div className="card-author-info">
-                    <span className="card-author">
-                      From. <span className="card-user">2파트 1팀</span>
-                      <span className="card-relationship-friend">친구</span>
-                    </span>
-                  </div>
-                </div>
-                <p className="Home-card-content">
-                  이성준 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  김교연 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  이현선 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  김경민 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  홍보희 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                </p>
-                <span className="card-date">2025.1.29</span>
-              </div>
-
-              {/* 두 번째 카드 */}
-              <div className="home-card">
-                <div className="card-header">
-                  <div className="card-profile-image">
-                    <img src={main_ex2} alt="프로필2" />
-                  </div>
-                  <div className="card-author-info">
-                    <span className="card-author">
-                      From. <span className="card-user"> 2파트 1팀</span>
-                      <span className="card-relationship">동료</span>
-                    </span>
-                  </div>
-                </div>
-                <p className="Home-card-content">
-                  이성준 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  김교연 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  이현선 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  김경민 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                  <br />
-                  홍보희 : ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                </p>
-
-                <span className="card-date">2025.01.30</span>
-              </div>
-
-              {/* + 버튼 카드 , To page연결*/}
-              <Link to="/post" className="card-placeholder">
-                +
-              </Link>
+        <section className="emoji-section">
+          {/* 추가된 클래스 'emoji-layout'를 통해 별도 스타일 적용 */}
+          <div className="point-container emoji-layout2">
+            {/* 왼쪽에 위치할 이미지 영역 */}
+            <div className="emoji-image">
+              <img src={CardImg} alt="이모티콘 이미지" />
+            </div>
+            {/* 오른쪽에 위치할 텍스트 영역 */}
+            <div className="emoji-text">
+              <h2 className="point-title">Point.01</h2>
+              <h3 className="point-subtitle">
+                누구나 손쉽게,온라인 <br />
+                롤링 페이퍼를 만들 수 있어요
+              </h3>
+              <p className="point-description">
+                로그인 없이 자유롭게 만들어요.
+              </p>
             </div>
           </div>
         </section>
