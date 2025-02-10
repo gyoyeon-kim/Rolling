5ccbf04749fca3848427cecd70fac9cd043708e8
Seongjun <drct9810@naver.com>
2025-02-10 21:03:52 +0900

    반응형 최종

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 import Navigation from "../component/Navigation";
 /* import HeaderBH from "../ComponentsBH/HeaderBH";
 /*import EmojiButton from "../component/EmojiButton";*/
-import point02 from "../images/Point02.png";
+import point02 from "../images/Group2 43 (1).png";
 import main_ex from "../images/main-ex1.jpg";
 import main_ex2 from "../images/main-ex2.jpg";
 import CardImg from "../images/CardList.svg";
@@ -21,19 +21,20 @@ function Home() {
           {/* 추가된 클래스 'emoji-layout'를 통해 별도 스타일 적용 */}
           <div className="point-container emoji-layout2">
             {/* 왼쪽에 위치할 이미지 영역 */}
-            <div className="emoji-image">
-              <img src={CardImg} alt="이모티콘 이미지" />
-            </div>
+
             {/* 오른쪽에 위치할 텍스트 영역 */}
             <div className="emoji-text">
               <h2 className="point-title">Point.01</h2>
               <h3 className="point-subtitle">
-                누구나 손쉽게,온라인 <br />
+                누구나 손쉽게, 온라인 <br />
                 롤링 페이퍼를 만들 수 있어요
               </h3>
               <p className="point-description">
                 로그인 없이 자유롭게 만들어요.
               </p>
+            </div>
+            <div className="emoji-image">
+              <img src={CardImg} alt="이모티콘 이미지" />
             </div>
           </div>
         </section>
@@ -44,18 +45,20 @@ function Home() {
           {/* 추가된 클래스 'emoji-layout'를 통해 별도 스타일 적용 */}
           <div className="point-container emoji-layout">
             {/* 왼쪽에 위치할 이미지 영역 */}
-            <div className="emoji-image">
-              <img src={point02} alt="이모티콘 이미지" />
-            </div>
+
             {/* 오른쪽에 위치할 텍스트 영역 */}
             <div className="emoji-text">
               <h2 className="point-title">Point.02</h2>
               <h3 className="point-subtitle">
-                서로에게 이모지로 감정을 표현해보세요
+                서로에게 이모지로 감정을
+                <br /> 표현해보세요
               </h3>
               <p className="point-description">
-                롤링 페이퍼에 이모지를 추가할 수 있어요!
+                롤링 페이퍼에 이모지를 추가할 수 있어요.
               </p>
+            </div>
+            <div className="emoji-image">
+              <img src={point02} alt="이모티콘 이미지" />
             </div>
           </div>
           <div className="explore section">
